bc45e4227f75b7bf5c697b9e6a2a6b3470dc2bce
Peter Hadlaw <hadlawp@gmail.com>
2014-02-09 13:18:52 -0600

    Added the 'flatly' bootswatch theme

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<title>{{$title}}</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	{{ HTML::style('css/bootstrap.css') }}
+	{{ HTML::style('css/bootswatch.min.css') }}
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     
 </head>
